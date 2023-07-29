aacb4bbd09af1844f8044b238ff2eec2de3ae228
Yusuf <kurakiorg@gmail.com>
2023-07-29 09:32:07 +0300

    Media Urls

--- main/urls.py
+++ main/urls.py
@@ -6,3 +6,7 @@ urlpatterns = [
     path('user/', include('user.urls')),
     path('api/', include('blog.urls')),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
