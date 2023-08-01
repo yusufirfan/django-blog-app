3147cab32ad0111068c72a9314e1d397c86aa643
Yusuf <kurakiorg@gmail.com>
2023-08-01 13:32:18 +0300

    Serializer.py

--- blog/serializers.py
+++ blog/serializers.py
@@ -8,3 +8,45 @@ class FixSerializer(serializers.ModelSerializer):
         validated_data['user_id'] = self.context['request'].user.id
         return super().create(validated_data)
     
+from .models import Category, Post, Comment, Like
+    
+class CategorySerializer(FixSerializer):
+    category_total = serializers.SerializerMethodField()
+
+    def get_category_total(self, obj):
+        return Category.objects.filter(id=obj.id).count()
+    
+    class Meta:
+        model = Category
+        exclude = []
+
+class PostSerializer(FixSerializer):
+    category = serializers.StringRelatedField()
+    likes = serializers.SerializerMethodField()
+    comments = serializers.SerializerMethodField()
+    like_total = serializers.SerializerMethodField()
+
+    def get_likes(self, obj):
+        return Like.objects.filter(id=obj.id)
+    
+    def get_comments(self, obj):
+        return Comment.objects.filter(id=obj.id)
+
+    def get_like_total(self, obj):
+        return Like.objects.filter(id=obj.id).count()
+    
+    class Meta:
+        model = Category
+        exclude = []
+
+class CommentSerializer(FixSerializer):
+    
+    class Meta:
+        model = Comment
+        exclude = []
+
+class LikeSerializer(FixSerializer):
+    
+    class Meta:
+        model = Like
+        exclude = []
