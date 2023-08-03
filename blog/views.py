e86c636a52fdbd62ed6727c18886d5c5cfc2730b
Yusuf <kurakiorg@gmail.com>
2023-08-03 12:38:08 +0300

    Views template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-# Create your views here.
+from .serializers import (Category, CategorySerializer,
+                          Post, PostSerializer,
+                          Comment, CommentSerializer,
+                          Like, LikeSerializer)
+
+class FixView(ModelViewSet):
+    pass
+
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user.is_staff
